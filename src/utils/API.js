e59be7606cc85676ea407a9131058e5538a2d173
Alex Brohman <alexbrohman9@gmail.com>
2019-05-30 10:47:22 -0400

    secure api

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://hn.algolia.com/api/v1/",
+    baseURL: "https://hn.algolia.com/api/v1/",
     responseType: "json"
 });
