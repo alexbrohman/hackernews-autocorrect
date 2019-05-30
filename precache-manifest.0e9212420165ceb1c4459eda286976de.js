ea6a7e857bfc9ddaab2dc3bf62e0595051603ac4
Alex Brohman <alexbrohman9@gmail.com>
2019-05-30 11:11:38 -0400

    Updates

---
+++ precache-manifest.0e9212420165ceb1c4459eda286976de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014e6e3666884cc88d312d203e5c7330",
+    "url": "/hackernews-autocomplete/index.html"
+  },
+  {
+    "revision": "ccb67b3e8b8307946b47",
+    "url": "/hackernews-autocomplete/static/css/main.4d2c0f37.chunk.css"
+  },
+  {
+    "revision": "5991f2e3e9e465e0ff81",
+    "url": "/hackernews-autocomplete/static/js/2.3df76db1.chunk.js"
+  },
+  {
+    "revision": "ccb67b3e8b8307946b47",
+    "url": "/hackernews-autocomplete/static/js/main.80c2a048.chunk.js"
+  },
+  {
+    "revision": "1d4164979ee7cae75c5c",
+    "url": "/hackernews-autocomplete/static/js/runtime~main.c268744a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b1d59c3bdd2ca68c1c624f1dceefd983.js"
+  "/hackernews-autocomplete/precache-manifest.0e9212420165ceb1c4459eda286976de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hackernews-autocomplete/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
