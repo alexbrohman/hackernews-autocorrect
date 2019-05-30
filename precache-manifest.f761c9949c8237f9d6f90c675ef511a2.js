24a4c15bcdd4530e18d5e889a22e9a9db6e9cfbf
Alex Brohman <alexbrohman9@gmail.com>
2019-05-30 10:44:48 -0400

    Updates

---
+++ precache-manifest.f761c9949c8237f9d6f90c675ef511a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5597bb1cedc0289c911ea63ced59ba",
+    "url": "/hackernews-autosearch/index.html"
+  },
+  {
+    "revision": "4eda95fa0b2f8b235a9c",
+    "url": "/hackernews-autosearch/static/css/main.4d2c0f37.chunk.css"
+  },
+  {
+    "revision": "5991f2e3e9e465e0ff81",
+    "url": "/hackernews-autosearch/static/js/2.3df76db1.chunk.js"
+  },
+  {
+    "revision": "4eda95fa0b2f8b235a9c",
+    "url": "/hackernews-autosearch/static/js/main.ff1b2d9b.chunk.js"
+  },
+  {
+    "revision": "d4fc071141de2d9647a9",
+    "url": "/hackernews-autosearch/static/js/runtime~main.7e28aa70.js"
+  }
+]);
